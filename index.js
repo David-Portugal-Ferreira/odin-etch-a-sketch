816db9f0f79a49306c6112ae8d6cfd10666b09c8
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-07-22 16:43:53 +0100

    Fix div's not staying on the wrapper div

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-let divContainer = document.querySelector('#container');
+const divWrapper = document.querySelector('#wrapper');
+const buttonNumberOfDivs = document.createElement('button');
+let numberOfDivs = 100;
 
+const divWrapperWidth = divWrapper.offsetWidth / numberOfDivs;
+const divWrapperHeight = divWrapper.offsetHeight / numberOfDivs;
 
-for (let i = 0; i <= 16; i++) {
-    for (let j = 0; j <= 16; j++) {
-        const divGrid = document.createElement('div');
-        divGrid.classList = 'div-grid';
-        divGrid.addEventListener('mouseover', () => {
+for (let i = 0; i < numberOfDivs; i++) {
+    let divContainer = document.createElement('div');
+    divContainer.classList = 'div-container';
+    divContainer.style.height = `${divWrapperHeight}px`;
+    for (let j = 0; j < numberOfDivs; j++) {
+        let divGrid = document.createElement('div');
+        divGrid.classList = 'div-grid'
+        divGrid.style.width = `${divWrapperWidth}px`;
+        divGrid.addEventListener('mouseenter', () => {
             divGrid.style['background-color'] = 'black';
-        })
+        });
         divContainer.appendChild(divGrid);
     }
+    divWrapper.appendChild(divContainer);
 }
