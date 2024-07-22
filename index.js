7bb1db37d54646d4cf5e9b6a48a6bf3ba5b967d9
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-07-22 17:12:11 +0100

    Add button logic to change grid layout

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 const divWrapper = document.querySelector('#wrapper');
+
 const buttonNumberOfDivs = document.createElement('button');
-let numberOfDivs = 100;
+buttonNumberOfDivs.addEventListener('click', getNumberOfDivs);
+buttonNumberOfDivs.textContent = 'Alter grid layout';
+buttonNumberOfDivs.classList = 'button-style';
+divWrapper.appendChild(buttonNumberOfDivs);
 
-const divWrapperWidth = divWrapper.offsetWidth / numberOfDivs;
-const divWrapperHeight = divWrapper.offsetHeight / numberOfDivs;
+let numberOfDivs = 16;
+constructGridLayout();
+//window.onload = constructGridLayout();
 
-for (let i = 0; i < numberOfDivs; i++) {
-    let divContainer = document.createElement('div');
-    divContainer.classList = 'div-container';
-    divContainer.style.height = `${divWrapperHeight}px`;
-    for (let j = 0; j < numberOfDivs; j++) {
-        let divGrid = document.createElement('div');
-        divGrid.classList = 'div-grid'
-        divGrid.style.width = `${divWrapperWidth}px`;
-        divGrid.addEventListener('mouseenter', () => {
-            divGrid.style['background-color'] = 'black';
-        });
-        divContainer.appendChild(divGrid);
+function constructGridLayout() {
+    const divWrapperWidth = divWrapper.offsetWidth / numberOfDivs;
+    const divWrapperHeight = divWrapper.offsetHeight / numberOfDivs;
+    for (let i = 0; i < numberOfDivs; i++) {
+        let divContainer = document.createElement('div');
+        divContainer.classList = 'div-container';
+        divContainer.style.height = `${divWrapperHeight}px`;
+        for (let j = 0; j < numberOfDivs; j++) {
+            let divGrid = document.createElement('div');
+            divGrid.classList = 'div-grid'
+            divGrid.style.width = `${divWrapperWidth}px`;
+            divGrid.addEventListener('mouseenter', () => {
+                divGrid.style['background-color'] = 'black';
+            });
+            divContainer.appendChild(divGrid);
+        }
+        divWrapper.appendChild(divContainer);
     }
-    divWrapper.appendChild(divContainer);
+}
+
+function getNumberOfDivs() {
+    numberOfDivs = 0;
+    while (numberOfDivs <= 15 || numberOfDivs >= 101) {
+        numberOfDivs = +prompt('Insert Grid Layout:');
+    }
+    let elementsToRemove = Array.from(getChildrenOfElement());
+    elementsToRemove.forEach(element => {
+        if(element.className === 'div-container') {
+            divWrapper.removeChild(element);
+        }
+    })
+    constructGridLayout();
+}
+
+function getChildrenOfElement() {
+    let childrenOfElement = divWrapper.childNodes;
+    return childrenOfElement;
 }
